d39d9a6ead839f6d533e5f0963df7c2a2ebd6a3a
Vanja Komadinovic <vanjakom@gmail.com>
2026-01-14 13:03:18 +0100

    0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.mungolab/osm-pss-integration "0.1.0-SNAPSHOT"
+(defproject com.mungolab/osm-pss-integration "0.1.0"
   :url "https://github.com/vanjakom/osm-pss-integration"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
